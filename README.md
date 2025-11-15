b2c256bbc889afb7b10735a1678cad373d0a346c
IszuHu <louwey.hu@gmail.com>
2025-11-15 19:08:22 +0100

    van nl->fr

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # leren
+* [x] alle woorden vragen
+* [x] van nl -> fr en terug
 * [ ] blijf vragen totdat jij een goed antwoord op geef
 * [ ] random woorden vragen 
 * [ ] alleen fouten antwoorden op het einde vragen
--- nederlands_frans.py
+++ nederlands_frans.py
@@ -1,10 +1,22 @@
 woorden = {'wachten':'attendre',
            'verwarren':'confondre',
-           'overeenkomen/corresponderen':'correspondre'}
+           'overeenkomen':'correspondre',
+           'corresponderen':'correspondre'}
 
-for fr_woord in woorden:
-    nl = input(f"wat betekent {woorden[fr_woord]} in het Frans")
-    if nl == fr_woord:
-        print('goed gedaan.')
-    else:
-        print('dat is niet goed.')
+def van_fr_naar_nl():
+    for nl_woord in woorden:
+        nl = input(f"wat betekent {woorden[nl_woord]} in het Frans")
+        if nl == nl_woord:
+            print('goed gedaan.')
+        else:
+            print('dat is niet goed.')
+
+def van_nl_naar_fr():
+    for nl_woord in woorden:
+        fr = input(f"wat betekent {nl_woord} in het Frans")
+        if fr == woorden[nl_woord]:
+            print('goed gedaan.')
+        else:
+            print('dat is niet goed.')
+
+van_nl_naar_fr()
