9b04f4fe2eef87f32622fc7a15bd16e28fe3de02
IszuHu <louwey.hu@gmail.com>
2025-11-15 18:45:15 +0100

    eerste werkende versie

---
+++ nederlands_frans.py
@@ -0,0 +1,8 @@
+woorden = {'wachten':'attendre',
+           'verwarren':'confondre',
+           'overeenkomen/corresponderen':'correspondre'}
+
+if input(f"wat betekent {woorden['wachten']} in het Frans") == 'wachten':
+    print('goed gedaan!')
+else:
+    print('dat is niet goed!')
