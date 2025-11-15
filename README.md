ca4465722a75b2ba97df3bc9985e87331f81fa9c
IszuHu <louwey.hu@gmail.com>
2025-11-15 19:25:58 +0100

    van nl->fr

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # leren
 * [x] alle woorden vragen
 * [x] van nl -> fr en terug
+* [x] random woorden vragen 
 * [ ] blijf vragen totdat jij een goed antwoord op geef
-* [ ] random woorden vragen 
 * [ ] alleen fouten antwoorden op het einde vragen
--- nederlands_frans.py
+++ nederlands_frans.py
@@ -1,20 +1,25 @@
-woorden = {'wachten':'attendre',
+import random
+fr_woord = {'wachten':'attendre',
            'verwarren':'confondre',
            'overeenkomen':'correspondre',
            'corresponderen':'correspondre'}
 
 def van_fr_naar_nl():
-    for nl_woord in woorden:
-        nl = input(f"wat betekent {woorden[nl_woord]} in het Frans")
+    woorden = list(fr_woord.keys())
+    random.shuffle(woorden)
+    for nl_woord in fr_woord:
+        nl = input(f"wat betekent {fr_woord[nl_woord]} in het Frans")
         if nl == nl_woord:
             print('goed gedaan.')
         else:
             print('dat is niet goed.')
 
 def van_nl_naar_fr():
+    woorden=list(fr_woord.keys())
+    random.shuffle(woorden)
     for nl_woord in woorden:
-        fr = input(f"wat betekent {nl_woord} in het Frans")
-        if fr == woorden[nl_woord]:
+        fr = input(f"wat betekent {nl_woord} in het Frans \n")
+        if fr == fr_woord[nl_woord]:
             print('goed gedaan.')
         else:
             print('dat is niet goed.')
